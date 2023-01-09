946444a0eee06da1d803f7f6978bb58fcf9692e1
smwhetten98 <smwhetten@gmail.com>
2023-01-09 09:53:23 -0700

    Completed Prep1 and Prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+class Program {
+    static void Main(string[] args) {
+/*
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your first name? ");
+        string lastName = Console.ReadLine();
+*/
+        string firstName = Question("What is your first name? ");
+        string lastName = Question("What is your last name? ");
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
+
+    static string Question(string query) {
+        Console.Write($"{query}");
+        return Console.ReadLine();
+    }
+
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade percentage? ");
+        string percentString = Console.ReadLine();
+        float percent = float.Parse(percentString);
+
+        string letterGrade;
+        if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        string passedMessage;
+        if (percent >= 70)
+        {
+            passedMessage = "Congradulations for passing this class!";
+        }
+        else
+        {
+            passedMessage = "Sorry, you did not pass this class this semester. Try again soon!";
+        }
+
+        string letterGradeSpecific = "";
+        if ((percent >= 60 && percent < 90) && percent % 10 >= 7)
+        {
+            letterGradeSpecific = "+";
+        }
+        else if (percent >= 60 && percent % 10 < 3)
+        {
+            letterGradeSpecific = "-";
+        }
+        letterGrade += letterGradeSpecific;
+
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+        Console.WriteLine(passedMessage);
     }
 }
