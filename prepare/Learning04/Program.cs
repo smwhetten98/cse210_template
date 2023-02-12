3e04f6735aa5466838738b5e724c304f00eb35df
smwhetten98 <smwhetten@gmail.com>
2023-02-12 13:17:09 -0700

    Completed Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment mathAssignment = new MathAssignment("Tom", "Calculus", "Section 8.4", "Problems 1-3");
+        WritingAssignment writingAssignment = new WritingAssignment("Susie", "Advanced Writing", "Writing in Business");
+
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class WritingAssignment: Assignment
+{
+	private string _title;
+
+	public WritingAssignment(string name, string topic, string title): base(name, topic)
+	{
+		_title = title;
+	}
+	public string GetWritingInformation()
+	{
+		return $"{base.GetSummary()}\n{_title} by {base.GetName()}";
+	}
+}
